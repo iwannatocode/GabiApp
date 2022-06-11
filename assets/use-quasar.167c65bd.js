83d7ae73d695010e0ba38539322931a6716f1d1d
jose <josemanuel94@gmail.com>
2022-06-10 23:50:51 -0400

    v 0.1.0

--- assets/use-quasar.167c65bd.js
+++
@@ -1,1 +0,0 @@
-import{d as a,a0 as r}from"./index.1568631b.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar.91d00b86.js
@@ -0,0 +1,1 @@
+import{d as a,a0 as r}from"./index.418386bc.js";function u(){return a(r)}export{u};
